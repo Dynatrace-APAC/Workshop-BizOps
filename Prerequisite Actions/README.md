d33923c50e65e5a7e7564a3e16082ffb3e0951c3
KevLeng <kevin.leng@dynatrace.com>
2020-02-04 13:48:44 +1100

    Updated

--- Prerequisite Actions/README.md
+++ Prerequisite Actions/README.md
@@ -1,6 +1,8 @@
-# Prerequisites Actions for the Dynatrace DEM Workshop 
+# Prerequisites Actions for the Dynatrace BizOps Workshop 
 
-The following conatins the instructions to create the easyTravel application which will be used duing the DEM workshop.
+## Create easyTravel Application
+
+The following conatins the instructions to create the easyTravel application which will be used during this workshop.
 
 Access your AWS console and navigate to the **EC2** service.
 
@@ -24,6 +26,26 @@ Use the defaults to create the instance except:
 Create a new key pair or use an existing one. **Please note: you will need the PEM key to access the instance via SSH.**
 
 Launch the instance...
+
+Wait for the Status Checks to read 2/2 checks 
+
+
+## Deploy Oneagent and Application Configuration to Dynatrace 
+
+Clone this repository to the server
+
+```bash
+$  git clone https://github.com/Dynatrace-APAC/Workshop-BizOps
+Cloning into 'Workshop-BizOps'...
+remote: Enumerating objects: 108, done.
+remote: Counting objects: 100% (108/108), done.
+remote: Compressing objects: 100% (85/85), done.
+remote: Total 108 (delta 19), reused 99 (delta 10), pack-reused 0
+Receiving objects: 100% (108/108), 5.78 MiB | 4.10 MiB/s, done.
+Resolving deltas: 100% (19/19), done.
+
+```
+
 
 ---
 :arrow_up_small: [Back to overview](/README.md)
---
+++ util/defineCredentials.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+YLW='\033[1;33m'
+NC='\033[0m'
+
+CREDS=./creds.json
+rm $CREDS 2> /dev/null
+
+echo -e "${YLW}Please enter the credentials as requested below: ${NC}"
+read -p "Dynatrace Tenant ID (ex. https://<TENANT_ID>.live.dynatrace.com or https://<TENANT_ID>.dynatrace-managed.com): " DTTEN
+read -p "Dynatrace Environment ID (Dynatrace Managed Only - https://<TENANT_ID>.dynatrace-managed.com/e/<ENVIRONMENT_ID>): " DTENV
+read -p "Dynatrace API Token: " DTAPI
+read -p "Dynatrace PaaS Token: " DTPAAS
+#read -p "GitHub User Name: " GITU 
+#read -p "GitHub Personal Access Token: " GITAT
+#read -p "GitHub User Email: " GITE
+#read -p "GitHub Organization: " GITO
+echo ""
+
+
+echo ""
+echo -e "${YLW}Please confirm all are correct: ${NC}"
+echo "Dynatrace Tenant ID: $DTTEN"
+echo "Dynatrace Environment ID: $DTENV"
+echo "Dynatrace API Token: $DTAPI"
+echo "Dynatrace PaaS Token: $DTPAAS"
+#echo "GitHub User Name: $GITU"
+#echo "GitHub Personal Access Token: $GITAT"
+#echo "GitHub User Email: $GITE"
+#echo "GitHub Organization: $GITO" 
+read -p "Is this all correct? (y/n) : " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm $CREDS 2> /dev/null
+    cat ./creds.sav | sed 's~DYNATRACE_TENANT_ID~'"$DTTEN"'~' | \
+      sed 's~DYNATRACE_ENVIRONMENT_ID~'"$DTENV"'~' | \
+      sed 's~DYNATRACE_API_TOKEN~'"$DTAPI"'~' | \
+      sed 's~DYNATRACE_PAAS_TOKEN~'"$DTPAAS"'~' >> $CREDS
+      #sed 's~GITHUB_USER_NAME_PLACEHOLDER~'"$GITU"'~' | \
+      #sed 's~PERSONAL_ACCESS_TOKEN_PLACEHOLDER~'"$GITAT"'~' | \
+      #sed 's~GITHUB_USER_EMAIL_PLACEHOLDER~'"$GITE"'~' | \
+      #sed 's~GITHUB_ORG_PLACEHOLDER~'"$GITO"'~' >> $CREDS
+fi
+
+cat $CREDS
+echo ""
+echo "The credentials file can be found here:" $CREDS
+echo ""
