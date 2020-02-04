5612f8b89031ee7be4189cc1378526f0b0171ca2
KevLeng <kevin.leng@dynatrace.com>
2020-02-04 15:33:44 +1100

    Updated

--- util/deployDynatrace.sh
+++ util/deployDynatrace.sh
@@ -48,7 +48,7 @@ deployOneAgent()
 
 deployApplicationConfig()
 {
-	export PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+	export PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 	export PRIVATE_HOSTNAME=$(hostname)
 	
 	## Create Application
