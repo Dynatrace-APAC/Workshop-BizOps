7f6780755434eaa36c72b326db70df6ab7e213ef
Kevin Leng <kevin.leng@dynatrace.com>
2020-06-05 12:45:41 +1000

    modified to deploy Synthetics only

--- util/deploySynthetics.sh
+++ util/deploySynthetics.sh
@@ -11,46 +11,10 @@ echo "TENANT = $TENANT"
 pwd
 
 
-deployApplicationConfig()
+deploySyntheticConfig()
 {
-	cd Workshop-BizOps/util
 	
 	export PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-	export PRIVATE_HOSTNAME=$(hostname)
-	
-	## Create Application
-	echo "Creating application in Dynatrace..."
-		
-	export APPLICATION_ID=$(curl -sX POST "https://"$TENANT".live.dynatrace.com/api/config/v1/applications/web" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token "$APITOKEN -H "Content-Type: application/json; charset=utf-8" -d @json/application.json | jq -r '.id')
-
-	echo "Application ID: $APPLICATION_ID"
-	
-	echo "Application created."
-	
-	#APPLICATION-6D7CE8352E9AD9BC
-	##curl http://169.254.169.254/latest/meta-data/public-hostname
-	##ec2-xxx-xxx-xxx-xxx.ap-southeast-2.compute.amazonaws.com
-
-	## Create Detection Rule
-	echo "Creating application detection rule in Dynatrace..."
-	
-
-	cp json/detectionrule.json json/privatedetectionrule.json 
-	cp json/detectionrule.json json/publicdetectionrule.json 
-
-	sed -i 's/"applicationIdentifier": "APPLICATION_ID",/"applicationIdentifier": "'$APPLICATION_ID'",/' json/privatedetectionrule.json	
-	sed -i 's/"applicationIdentifier": "APPLICATION_ID",/"applicationIdentifier": "'$APPLICATION_ID'",/' json/publicdetectionrule.json
-	
-	sed -i 's/"pattern": "PATTERN",/"pattern": "'$PRIVATE_HOSTNAME'",/' json/privatedetectionrule.json
-	sed -i 's/"pattern": "PATTERN",/"pattern": "'$PUBLIC_HOSTNAME'",/' json/publicdetectionrule.json
-	
-
-	curl -sX POST "https://"$TENANT".live.dynatrace.com/api/config/v1/applicationDetectionRules/" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token "$APITOKEN -H "Content-Type: application/json; charset=utf-8" -d @json/privatedetectionrule.json
-	
-	curl -sX POST "https://"$TENANT".live.dynatrace.com/api/config/v1/applicationDetectionRules/" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token "$APITOKEN -H "Content-Type: application/json; charset=utf-8" -d @json/publicdetectionrule.json
-	
-	echo "Detection rules created."
-	
 	
 	## Synthetic Monitor...
 	echo "Creating sythetic monitor in Dynatrace..."
@@ -64,4 +28,4 @@ deployApplicationConfig()
 }
 
 
-deployApplicationConfig
+deploySyntheticConfig
